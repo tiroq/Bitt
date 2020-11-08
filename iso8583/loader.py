4908e2b121655b4cb141b3fbc032762fea220bd9
IShamrai <isshamrai@gmail.com>
2020-11-08 08:40:09 +0300

    Initial parsing
    Fix dictionary
    Read dict
    Initial parsing of MTI and bitmap

--- iso8583/loader.py
+++ iso8583/loader.py
@@ -1,1 +1,20 @@
+import os
 import yaml
+
+from collections import namedtuple
+
+Field = namedtuple("Field", ["FieldID", "Type", "MaxLen", "LenType", "Description"])
+
+class Config(object):
+    def __init__(self, config):
+        super().__init__()
+        if not os.path.exists(config):
+            print("Can't find configuration file.")
+        self.config = yaml.load(open(config))
+        fields = {}
+        for field in self.config['Fields']:
+            if isinstance(field, tuple):
+                fields[field[0]]=Field(*field)
+            else:
+                print("Unexpected field format:", field)
+        self.config['Fields'] = fields
