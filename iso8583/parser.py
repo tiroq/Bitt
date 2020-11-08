d4ebbae5fc622cc9e73a7dff8276bc09447f0ab7
IShamrai <isshamrai@gmail.com>
2020-11-08 13:29:56 +0300

    Add MTI reading to Parser

--- iso8583/parser.py
+++ iso8583/parser.py
@@ -2,3 +2,9 @@
 class ISO8583Parser(object):
     def __init__(self, config):
         self.__cfg = config
+    
+    def parse(self, msg):
+        mti, position = self.__getMTI(msg)
+    
+    def __getMTI(self, msg):
+        return msg[0:self.__cfg.mti.Length], self.__cfg.mti.Length
