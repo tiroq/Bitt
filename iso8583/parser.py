2dc9bdfebdc4896a890360be1661a66f69766d2f
IShamraI <isshamray@gmail.com>
2020-11-08 14:05:54 +0300

    Add Bitmap parsing

--- iso8583/parser.py
+++ iso8583/parser.py
@@ -1,13 +1,27 @@
+from .loader import Config
 
 class ISO8583Parser(object):
     def __init__(self, config):
-        self.__cfg = config
+        self.__cfg = Config(config)
     
     def parse(self, msg):
-        mti, position = self.__getMTI(msg)
+        mti, position = self._getMTI(msg)
+        bitmap, position = self._getBitmap(msg, position)
+        return mti, bitmap
     
-    def __getMTI(self, msg):
-        return msg[0:self.__cfg.mti.Length], self.__cfg.mti.Length
+    def _getMTI(self, msg):
+        return msg[0:self.__cfg.mti.Length].decode("utf-8") , self.__cfg.mti.Length
 
-    def __getBitmap(self, msg, position):
+    def _getBitmap(self, msg, position):
         BitmapFieldID = 1  # TODO: move?
+        end_position = position + self.__cfg.fields[BitmapFieldID].MaxLen
+        flags = msg[position:end_position]
+        # Make Bitmap
+        _bits = [flags[i//8] & 1 << i%8 != 0 for i in range(len(flags) * 8)]
+        # change order of bits foreach byte
+        bits = []
+        for i in range(int(len(_bits)/8)):
+            bits += _bits[i*8:i*8+8][::-1] 
+
+        bitmap = [ i + 1 for i, b in enumerate(bits) if b ]
+        return bitmap, end_position
--- test.py
+++ test.py
@@ -1,30 +1,12 @@
 import yaml
 from pprint import pprint
-from iso8583.loader import Config
+
+# from iso8583.loader import Config
+from iso8583.parser import ISO8583Parser
 
 msg = bytes.fromhex('303230307234000008C08000313634303030303130303030303030303031383430303030303030303030303031303030313130363134303131373333343734383230313130363134303131373235313230333131313433333437343830303939393230314D3939393230312020202020202020383130')
 
-cfg = Config('dicts/sample.yaml')
-print(cfg.config)
-print(msg)
-p = 0
-print(msg[p:p + cfg.config['MTI']['Size']])
-p += cfg.config['MTI']['Size']
-# b = bytearray(msg[p:p + cfg.config['Bitmap']['Size']])
-flags = msg[p:p + cfg.config['Bitmap']['Size']]
-# Make Bitmap
-_bits =  [flags[i//8] & 1 << i%8 != 0 for i in range(len(flags) * 8)]
-#change order of bits
-bits = []
-for i in range(int(len(_bits)/8)):
-    bits += _bits[i*8:i*8+8][::-1]
-print(len(bits))
-bitmap = []
-for i, b in enumerate(bits):
-    if b:
-        print(i+1, b)
-        bitmap.append(i + 1)
-print(bitmap)
-# print('7234000008C08000')
-# print(bytes.fromhex('7234000008C08000'))
-# print(bytearray(msg[p:p + cfg.config['Bitmap']['Size']]))
+parser = ISO8583Parser('dicts/sample.yaml')
+# print(cfg.fields)
+# print(msg)
+print(parser.parse(msg))
